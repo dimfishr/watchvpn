0524e08c4205b38032e23f72a4f25e38c17b99d7
dimfishr <dimfish@gmail.com>
2020-05-22 12:33:43 +0300

    Improve time
    standalone config file

--- usr/bin/watchvpn.sh
+++ usr/bin/watchvpn.sh
@@ -5,48 +5,48 @@
 # This is free software, licensed under the GNU General Public License v2.
 #
 
-watchvpn_ping() {
+watchvpn() {
 	local period="$1"; local pinghosts="$2"; local ifname="$3"; local pingperiod="$4"
 
-	time_now="$(cat /proc/uptime)"
-	time_now="${time_now%%.*}"
-	time_lastcheck="$time_now"
-	time_lastcheck_withinternet="$time_now"
+	time_chk=$(time_now)
+	time_chk_inet=$time_chk
 
 	while true
 	do
-		time_now="$(cat /proc/uptime)"
-		time_now="${time_now%%.*}"
-		time_diff="$((time_now-time_lastcheck))"
+		time_diff="$(($(time_now)-time_chk))"
 
 		[ "$time_diff" -lt "$pingperiod" ] && {
 			sleep_time="$((pingperiod-time_diff))"
 			sleep "$sleep_time"
 		}
 
-		time_now="$(cat /proc/uptime)"
-		time_now="${time_now%%.*}"
-		time_lastcheck="$time_now"
+		time_chk=$(time_now)
 
 		for host in $pinghosts
 		do
 			if ping -c 1 "$host" &> /dev/null
 			then
-				time_lastcheck_withinternet="$time_now"
+				time_chk=$(time_now)
+				time_chk_inet=$time_chk
 			else
-				time_diff="$((time_now-time_lastcheck_withinternet))"
-				logger -p daemon.info -t "watchvpn[$$]" "no VPN connectivity for $time_diff seconds. Reseting when reaching $period"
+				time_diff="$(($(time_now)-time_chk_inet))"
+				logger -p daemon.info -t "watchvpn[$$]" "no $ifname connectivity for $time_diff seconds. Reseting when reaching $period"
 			fi
 		done
 
-		time_diff="$((time_now-time_lastcheck_withinternet))"
+		time_diff="$(($(time_now)-time_chk_inet))"
 		[ "$time_diff" -ge "$period" ] && {
 			logger -p daemon.info -t "watchvpn[$$]" "Restarting $ifname"
 			ifdown "$ifname" && ifup "$ifname"
-			time_lastcheck="$time_now"
-			time_lastcheck_withinternet="$time_now"
+
+			time_chk=$(time_now)
+			time_chk_inet=$time_chk
 		}
 	done
 }
 
-watchvpn_ping "$1" "$2" "$3" "$4"
+time_now() {
+	echo "$(cut -f1 -d. /proc/uptime)"
+}
+
+watchvpn "$1" "$2" "$3" "$4"
